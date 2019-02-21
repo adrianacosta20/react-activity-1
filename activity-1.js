f6e7e7024758f82b6683238e4b9d685bf738db6c
Adrian Acosta <aacosta@techlaunch>
2019-02-21 18:22:10 -0500

    assigment done

---
+++ activity-1.js
@@ -0,0 +1,23 @@
+class Developer{
+    constructor(first_name,last_name){
+        this.first_name = first_name;
+        this.last_name = last_name;
+    };
+    getName(){
+        return `${this.first_name} ${this.last_name}`;
+    };
+};
+
+class JavaScriptDeveloper extends Developer{
+    getJob(){
+        return 'JavaScript Developer';
+    };
+};
+
+var dev = new JavaScriptDeveloper('Adrian','Acosta');
+console.log(dev.getName());
+console.log(dev.getJob());
+
+var getGreeting = () =>{
+    return 'Welcome to React Dev';
+};
